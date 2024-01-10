3bfa91b5888a15840a5a2f7cb8aa291583076809
juanpahv <dcrunneryt1@gmail.com>
2024-01-09 19:57:36 -0600

    add projects section (test)

---
+++ src/components/Projects.tsx
@@ -0,0 +1,35 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
+
+function Projects() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+                <CardFooter className="flex justify-center">
+                  <span>test</span>
+                </CardFooter>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
+export default Projects;
