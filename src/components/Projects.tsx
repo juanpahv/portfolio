c5fa9760845f37c585251e14d3a0a4135da261a7
juanpahv <dcrunneryt1@gmail.com>
2024-02-15 15:23:32 -0600

    add projects and fix pills

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,16 @@
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Card, CardContent, CardFooter } from "@/components/ui/card"
+import CarouselCard from "@/components/CarouselCard";
 
 function Projects() {
   return (
-    <Carousel className="w-full max-w-xs">
+    <Carousel className="w-full">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-                <CardFooter className="flex justify-center">
-                  <span>test</span>
-                </CardFooter>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
+        <CarouselCard />
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
